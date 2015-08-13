9b5115f6f5be9d725b68ed899c20aaa15dc5e09f
Michael Waskom <mwaskom@stanford.edu>
2015-08-13 16:14:20 -0700

    Add CNI projector system

--- monitors.py
+++ monitors.py
@@ -1,7 +1,7 @@
 """Hold information about different monitors."""
 from textwrap import dedent
 
-cni_30 = dict(monitor_name='cni_30',
+cni_30 = dict(name='cni_30',
               calib_file='calib/cni_lums_20110718.csv',
               calib_date='20110718',
               width=64.3,
@@ -14,26 +14,32 @@ cni_30 = dict(monitor_name='cni_30',
               Accessed on 8/9/2011.
               """))
 
-cni_47 = dict(monitor_name='cni_47',
+cni_47 = dict(name='cni_47',
               width=103.8,
               distance=277.1,
               size=[1920, 1080],
-              notes=dedent('47" 3D LCD display - not yet calibrated'))
+              notes=dedent('47" 3D LCD display at the back of the bore'))
 
-mlw_mbair = dict(monitor_name='mlw-mbair',
+cni_projector = dict(name='cni_projector',
+                     width=58,
+                     distance=54,
+                     size=[1920, 1080],
+                     notes=dedent('Calibration info from Dan Birman'))
+
+mlw_mbair = dict(name='mlw-mbair',
                  width=30.5,
                  size=[1440, 900],
                  distance=63,
                  notes="")
 
-mwmp = dict(monitor_name='mwmp',
+mwmp = dict(name='mwmp',
             width=50,
             distance=55,
             size=[1680, 1050],
             notes="This is the Dell monitor on my office computer")
 
-waglab_mbpro = dict(monitor_name='waglab-mbpro',
+waglab_mbpro = dict(name='waglab-mbpro',
                     width=33,
                     size=[1440, 900],
                     distance=63,
-                    notes="")
+                    notes="This should be true of both Curtis and Ari")
