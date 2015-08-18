0468c02d0d01392abc2f3d1d25dbec3ab8218b0b
Michael Waskom <mwaskom@stanford.edu>
2015-08-18 15:55:48 -0700

    Add script to show a circle the size of the stimulus

---
+++ check_screen.py
@@ -0,0 +1,21 @@
+
+import sys
+from psychopy import visual, event
+import cregg
+
+
+def main(arglist):
+
+    p = cregg.Params("scan")
+    p.set_by_cmdline(arglist)
+    win = cregg.launch_window(p)
+    visual.Circle(win, p.array_radius,
+                  edges=128,
+                  lineColor="white",
+                  lineWidth=2).draw()
+    win.flip()
+    event.waitKeys(p.quit_keys)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
