1d6c18c8c103771e4c922d8be0ae3fb48c4df80b
mwaskom <mwaskom@stanford.edu>
2015-08-18 19:58:31 -0700

    Add script to show a fixation cross

---
+++ fixation.py
@@ -0,0 +1,17 @@
+
+import sys
+import cregg
+
+
+def main(arglist):
+
+    p = cregg.Params("scan")
+    p.set_by_cmdline(arglist)
+    win = cregg.launch_window(p)
+    cregg.WaitText(win, "+", height=2,
+                   advance_keys=[],
+                   quit_keys=p.quit_keys).draw()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
