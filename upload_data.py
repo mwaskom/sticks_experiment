2009d41453b80b91800ef1c8598db0874b2c5960
Michael Waskom <mwaskom@stanford.edu>
2015-08-19 10:49:51 -0700

    Upload data to the desktop

--- upload_data.py
+++ upload_data.py
@@ -1,5 +1,5 @@
 import os
 import sys
 subj = sys.argv[1]
-target = "mwaskom@mwmp:studies/squad/data/{}_behav".format(subj)
+target = "mwaskom@mwmp:Desktop/{}_behav".format(subj)
 os.system("rsync -aP data/{}_* {}".format(subj, target))
