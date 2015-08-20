184023884a5d64af5f303adf895cd82eadce2442
Michael Waskom <mwaskom@stanford.edu>
2015-08-19 17:45:41 -0700

    Use a black cross for resting state

--- fixation.py
+++ fixation.py
@@ -8,9 +8,8 @@ def main(arglist):
     p = cregg.Params("scan")
     p.set_by_cmdline(arglist)
     win = cregg.launch_window(p)
-    cregg.WaitText(win, "+", height=2,
-                   advance_keys=[],
-                   quit_keys=p.quit_keys).draw()
+    cregg.WaitText(win, "+", height=2, color="black",
+                   advance_keys=[], quit_keys=p.quit_keys).draw()
 
 
 if __name__ == "__main__":
