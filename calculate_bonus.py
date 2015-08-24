ef9181cd0e564c6fc5a3d4b9010a9f6f8849aef1
Michael Waskom <mwaskom@stanford.edu>
2015-08-24 16:52:31 -0700

    Add script to calculate bonus

---
+++ calculate_bonus.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+import sys
+import pandas as pd
+
+
+def main(arglist):
+
+    subj = arglist[0]
+    template = "data/{}_scan_run{:02d}.csv"
+
+    dfs = []
+    for run in range(1, 13):
+        try:
+            run_df = pd.read_csv(template.format(subj, run))
+            dfs.append(run_df)
+        except IOError:
+            print("No data file for run {}".format(run))
+    df = pd.concat(dfs, ignore_index=True)
+
+    acc = df.correct.mean()
+    rt = df.rt.mean()
+
+    print("Subject {}:".format(subj))
+    print("Accuracy: {:.2f}".format(acc))
+    print("Mean RT: {:.2f}".format(rt))
+
+    bonus = acc / (rt * .5) * 15
+    print("Bonus: {:.2f}".format(bonus))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
